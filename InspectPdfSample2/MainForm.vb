86f2aee083d37deb54f95136529b13f5749e02af
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2018-05-28 23:12:48 +0900

    iText7 のサンプルを追加する

---
+++ InspectPdfSample2/MainForm.vb
@@ -0,0 +1,89 @@
+﻿Imports iText.Kernel.Geom
+Imports iText.Kernel.Pdf
+
+Public Class MainForm
+
+    Private ReadOnly _pdfPageSizeModel As PdfPageSizeModel = New PdfPageSizeModel()
+    Private ReadOnly _standardPageSizeModel As StandardPageSizeModel = New StandardPageSizeModel()
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        FileNameTextBox.DataBindings.Add(NameOf(TextBox.Text), _pdfPageSizeModel, NameOf(PdfPageSizeModel.FileName))
+
+        OpenPdfFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+
+        PdfWidthPointTextBox.DataBindings.Add(NameOf(TextBox.Text), _pdfPageSizeModel, NameOf(PdfPageSizeModel.WidthPoint))
+        PdfHeightPointTextBox.DataBindings.Add(NameOf(TextBox.Text), _pdfPageSizeModel, NameOf(PdfPageSizeModel.HeightPoint))
+        PdfWidthInchesTextBox.DataBindings.Add(NameOf(TextBox.Text), _pdfPageSizeModel, NameOf(PdfPageSizeModel.WidthInches))
+        PdfHeightInchesTextBox.DataBindings.Add(NameOf(TextBox.Text), _pdfPageSizeModel, NameOf(PdfPageSizeModel.HeightInches))
+        PdfWidthCmTextBox.DataBindings.Add(NameOf(TextBox.Text), _pdfPageSizeModel, NameOf(PdfPageSizeModel.WidthCm))
+        PdfHeightCmTextBox.DataBindings.Add(NameOf(TextBox.Text), _pdfPageSizeModel, NameOf(PdfPageSizeModel.HeightCm))
+
+        With PageSizeComboBox.Items
+            .Add(StandardPageSize.A0)
+            .Add(StandardPageSize.A1)
+            .Add(StandardPageSize.A2)
+            .Add(StandardPageSize.A3)
+            .Add(StandardPageSize.A4)
+            .Add(StandardPageSize.A5)
+            .Add(StandardPageSize.A6)
+            .Add(StandardPageSize.A7)
+            .Add(StandardPageSize.B0)
+            .Add(StandardPageSize.B1)
+            .Add(StandardPageSize.B2)
+            .Add(StandardPageSize.B3)
+            .Add(StandardPageSize.B4)
+            .Add(StandardPageSize.B5)
+            .Add(StandardPageSize.B6)
+            .Add(StandardPageSize.B7)
+        End With
+
+        StandardWidthPointTextBox.DataBindings.Add(NameOf(TextBox.Text), _standardPageSizeModel, NameOf(StandardPageSizeModel.WidthPointText))
+        StandardHeightPointTextBox.DataBindings.Add(NameOf(TextBox.Text), _standardPageSizeModel, NameOf(StandardPageSizeModel.HeightPoint))
+        StandardWidthInchesTextBox.DataBindings.Add(NameOf(TextBox.Text), _standardPageSizeModel, NameOf(StandardPageSizeModel.WidthInches))
+        StandardHeightInchesTextBox.DataBindings.Add(NameOf(TextBox.Text), _standardPageSizeModel, NameOf(StandardPageSizeModel.HeightInches))
+        StandardWidthCmTextBox.DataBindings.Add(NameOf(TextBox.Text), _standardPageSizeModel, NameOf(StandardPageSizeModel.WidthCm))
+        StandardHeightCmTextBox.DataBindings.Add(NameOf(TextBox.Text), _standardPageSizeModel, NameOf(StandardPageSizeModel.HeightCm))
+
+        PageSizeComboBox.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub OpenDialogButton_Click(sender As Object, e As EventArgs) Handles OpenDialogButton.Click
+        If OpenPdfFileDialog.ShowDialog() = DialogResult.OK Then
+            Using reader As New PdfReader(OpenPdfFileDialog.FileName)
+                Using document As New PdfDocument(reader)
+
+                    _pdfPageSizeModel.FileName = OpenPdfFileDialog.FileName
+                    Dim pageSize = GetPage(document)
+                    _pdfPageSizeModel.WidthPoint = pageSize.GetWidth()
+                    _pdfPageSizeModel.HeightPoint = pageSize.GetHeight()
+                End Using
+            End Using
+        End If
+
+    End Sub
+
+    Private Sub PageSizeComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) _
+        Handles PageSizeComboBox.SelectedIndexChanged
+
+        _standardPageSizeModel.PageSize = PageSizeComboBox.SelectedItem
+    End Sub
+
+    Private Function GetPage(document As PdfDocument) As Rectangle
+        If document.GetNumberOfPages() = 0 Then Return Nothing
+
+
+        Dim pageSize = document.GetFirstPage().GetPageSize()
+
+        Dim forDebug As New Dictionary(Of Integer, Rectangle)() From {{1, pageSize}}
+
+        For i = 2 To document.GetNumberOfPages()
+            If Not pageSize.EqualsWithEpsilon(document.GetPage(i).GetPageSize(), 1.0F) Then
+                forDebug.Add(i, document.GetPage(i).GetPageSize())
+            End If
+        Next
+
+        Return pageSize
+
+    End Function
+End Class
---
+++ InspectPdfSample2/StandardPageSize.vb
@@ -0,0 +1,75 @@
+﻿Imports iText.Kernel.Geom
+
+Public Class StandardPageSize
+    Public ReadOnly Property Size As Rectangle
+    Public ReadOnly Property Name As String
+
+    Public Shared A0 As StandardPageSize = New StandardPageSize("A0")
+    Public Shared A1 As StandardPageSize = New StandardPageSize("A1")
+    Public Shared A2 As StandardPageSize = New StandardPageSize("A2")
+    Public Shared A3 As StandardPageSize = New StandardPageSize("A3")
+    Public Shared A4 As StandardPageSize = New StandardPageSize("A4")
+    Public Shared A5 As StandardPageSize = New StandardPageSize("A5")
+    Public Shared A6 As StandardPageSize = New StandardPageSize("A6")
+    Public Shared A7 As StandardPageSize = New StandardPageSize("A7")
+
+    Public Shared B0 As StandardPageSize = New StandardPageSize("B0")
+    Public Shared B1 As StandardPageSize = New StandardPageSize("B1")
+    Public Shared B2 As StandardPageSize = New StandardPageSize("B2")
+    Public Shared B3 As StandardPageSize = New StandardPageSize("B3")
+    Public Shared B4 As StandardPageSize = New StandardPageSize("B4")
+    Public Shared B5 As StandardPageSize = New StandardPageSize("B5")
+    Public Shared B6 As StandardPageSize = New StandardPageSize("B6")
+    Public Shared B7 As StandardPageSize = New StandardPageSize("B7")
+
+
+    Private Sub New(pageSizeName As String)
+
+        Name = pageSizeName
+        Select Case pageSizeName
+            Case "A0"
+                Size = PageSize.A0
+            Case "A1"
+                Size = PageSize.A1
+            Case "A2"
+                Size = PageSize.A2
+            Case "A3"
+                Size = PageSize.A3
+            Case "A4"
+                Size = PageSize.A4
+            Case "A5"
+                Size = PageSize.A5
+            Case "A6"
+                Size = PageSize.A6
+            Case "A7"
+                Size = PageSize.A7
+
+            Case "B0"
+                Size = PageSize.B0
+            Case "B1"
+                Size = PageSize.B1
+            Case "B2"
+                Size = PageSize.B2
+            Case "B3"
+                Size = PageSize.B3
+            Case "B4"
+                Size = PageSize.B4
+            Case "B5"
+                Size = PageSize.B5
+            Case "B6"
+                Size = PageSize.B6
+            Case "B7"
+                Size = PageSize.B7
+
+            Case Else
+                Throw New ArgumentException()
+        End Select
+
+    End Sub
+
+    ''' <inheritdoc />
+    Public Overrides Function ToString() As String
+        Return Name
+    End Function
+
+End Class
